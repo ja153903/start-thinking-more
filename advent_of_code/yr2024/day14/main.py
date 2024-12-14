347e566fb1fca80566003d7e6fb3fe6b3c2ae529
ja153903 <jaimeabbariao@gmail.com>
2024-12-13 22:12:03 -0500

    updating some stuff

---
+++ advent_of_code/yr2024/day14/main.py
@@ -0,0 +1,20 @@
+import os
+
+PATH_TO_FILE = f"{os.path.dirname(os.path.realpath(__file__))}/data.in"
+
+
+def parse_input():
+    with open(PATH_TO_FILE, "r") as f:
+        data = f.read().strip()
+
+
+def part1(): ...
+
+
+def part2(): ...
+
+
+if __name__ == "__main__":
+    print("Advent of Code 2024 - Day 14")
+    print(f"Part 1: {part1()}")
+    print(f"Part 2: {part2()}")
