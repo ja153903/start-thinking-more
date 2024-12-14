97c878047ab11f34bd75efe744c998a73f384639
ja153903 <jaimeabbariao@gmail.com>
2024-12-14 01:34:29 -0500

    updating with solutions

--- advent_of_code/yr2024/day14/main.py
+++ advent_of_code/yr2024/day14/main.py
@@ -1,17 +1,161 @@
 import os
+import re
+import time
+from typing import TypedDict
+from collections import Counter
+
+
+def pretty_print(grid: list[list[int]]):
+    time.sleep(1)
+    for row in grid:
+        print("".join([str(item) if item > 0 else "." for item in row]))
+
 
 PATH_TO_FILE = f"{os.path.dirname(os.path.realpath(__file__))}/data.in"
+
+if "data.in" in PATH_TO_FILE:
+    Y_LIMIT = 103
+    X_LIMIT = 101
+
+    X_MIDPOINT = 50
+    Y_MIDPOINT = 51
+else:
+    Y_LIMIT = 7
+    X_LIMIT = 11
+
+    X_MIDPOINT = 5
+    Y_MIDPOINT = 3
+
+
+class Robot(TypedDict):
+    id: int
+    px: int
+    py: int
+    vx: int
+    vy: int
+
+
+# In this regex, we make sure to capture entries that can either be positive or negative
+ROBOT_REGEX = re.compile(r"p=(?P<px>-?\d+),(?P<py>-?\d+) v=(?P<vx>-?\d+),(?P<vy>-?\d+)")
 
 
 def parse_input():
     with open(PATH_TO_FILE, "r") as f:
-        data = f.read().strip()
+        data: list[Robot] = []
+        for i, line in enumerate(f.read().strip().split("\n")):
+            match = ROBOT_REGEX.match(line)
+            if match:
+                data.append(
+                    {
+                        "id": i,
+                        "px": int(match.group("px")),
+                        "py": int(match.group("py")),
+                        "vx": int(match.group("vx")),
+                        "vy": int(match.group("vy")),
+                    }
+                )
+            else:
+                raise ValueError("Could not parse data correctly; check regex")
+        return data
 
 
-def part1(): ...
+def get_grid():
+    return [[0 for _ in range(X_LIMIT)] for _ in range(Y_LIMIT)]
 
 
-def part2(): ...
+def move_robot(robot: Robot, x: int, y: int) -> tuple[int, int]:
+    vx, vy = robot["vx"], robot["vy"]
+
+    nx = x + vx
+    ny = y + vy
+
+    if nx < 0:
+        nx += X_LIMIT
+
+    if ny < 0:
+        ny += Y_LIMIT
+
+    return (nx % X_LIMIT, ny % Y_LIMIT)
+
+
+def determine_quadrant(row: int, col: int) -> str:
+    match (row, col):
+        case (r, c) if r < Y_MIDPOINT and c > X_MIDPOINT:
+            return "I"
+        case (r, c) if r < Y_MIDPOINT and c < X_MIDPOINT:
+            return "II"
+        case (r, c) if r > Y_MIDPOINT and c < X_MIDPOINT:
+            return "III"
+        case _:
+            return "IV"
+
+
+def part1():
+    grid = get_grid()
+    robots = parse_input()
+
+    for robot in robots:
+        grid[robot["py"]][robot["px"]] += 1
+
+    for _ in range(100):
+        for robot in robots:
+            x, y = robot["px"], robot["py"]
+            grid[y][x] -= 1
+
+            nx, ny = move_robot(robot, x, y)
+
+            robot["px"], robot["py"] = nx, ny
+            grid[ny][nx] += 1
+
+    count_by_quadrants = Counter()
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] > 0 and i != Y_MIDPOINT and j != X_MIDPOINT:
+                count_by_quadrants[determine_quadrant(i, j)] += grid[i][j]
+
+    res = 1
+
+    for count in count_by_quadrants.values():
+        res *= count
+
+    return res
+
+
+def only_ones(grid: list[list[int]]):
+    for row in grid:
+        for col in row:
+            if col > 1:
+                return False
+
+    return True
+
+
+# The intuition for part 2 is that we'll get some sort of tree if
+# all the robots are in their own independent positions
+def part2():
+    grid = get_grid()
+    robots = parse_input()
+
+    for robot in robots:
+        grid[robot["py"]][robot["px"]] += 1
+
+    iteration = 0
+
+    while True:
+        for robot in robots:
+            x, y = robot["px"], robot["py"]
+            grid[y][x] -= 1
+
+            nx, ny = move_robot(robot, x, y)
+
+            robot["px"], robot["py"] = nx, ny
+            grid[ny][nx] += 1
+
+        iteration += 1
+
+        if only_ones(grid):
+            return iteration
 
 
 if __name__ == "__main__":
