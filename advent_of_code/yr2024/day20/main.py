e18135131315b469b43be1f8e1b476aad20c6213
ja153903 <jaimeabbariao@gmail.com>
2024-12-26 14:40:41 -0500

    adding solution for day 20

--- advent_of_code/yr2024/day20/main.py
+++ advent_of_code/yr2024/day20/main.py
@@ -1,9 +1,9 @@
 import os
 import heapq
 from typing import NamedTuple
-from collections import Counter
+from collections import defaultdict
 
-PATH_TO_FILE = f"{os.path.dirname(os.path.realpath(__file__))}/data.test.in"
+PATH_TO_FILE = f"{os.path.dirname(os.path.realpath(__file__))}/data.in"
 
 
 class RegularNode(NamedTuple):
@@ -52,7 +52,8 @@ def dijkstras(maze: list[list[str]]):
     q: list[RegularNode] = []
     heapq.heappush(q, RegularNode(0, start[0], start[1]))
 
-    visited = set()
+    dd = defaultdict(lambda: float("inf"))
+    dd[start] = 0
 
     while q:
         dist, x, y = heapq.heappop(q)
@@ -60,8 +61,6 @@ def dijkstras(maze: list[list[str]]):
         if (x, y) == end:
             return dist
 
-        visited.add((x, y))
-
         for dx, dy in DIRECTIONS:
             nx, ny = x + dx, y + dy
 
@@ -70,97 +69,95 @@ def dijkstras(maze: list[list[str]]):
                 or ny < 0
                 or nx >= len(maze[0])
                 or ny >= len(maze)
-                or (nx, ny) in visited
                 or maze[ny][nx] == "#"
             ):
                 continue
 
-            heapq.heappush(q, RegularNode(dist + 1, nx, ny))
+            alt = dist + 1
+            if alt <= dd[(nx, ny)]:
+                dd[(nx, ny)] = alt
+                heapq.heappush(q, RegularNode(alt, nx, ny))
+
+    raise ValueError("Could not find the optimal path")
 
 
 def part1():
     maze = parse_input()
-
     picosecond_without_cheats = dijkstras(maze)
 
-    counter = Counter()
+    res = 0
 
     for y in range(len(maze)):
         for x in range(len(maze[y])):
             if maze[y][x] == "#":
                 maze[y][x] = "."
-                with_cheats = dijkstras(maze)
-                if with_cheats is not None:
-                    counter[with_cheats] += 1
+                picoseconds = dijkstras(maze)
+                try:
+                    if picosecond_without_cheats - picoseconds >= 100:
+                        res += 1
+                except ValueError:
+                    pass
                 maze[y][x] = "#"
 
-    res = 0
-    for key, value in counter.items():
-        if picosecond_without_cheats - key >= 100:
-            res += value
     return res
 
 
-def part2():
-    # this is going to be similar to part 1, but instead of changing just one
-    # we have to construct a path
-    maze = parse_input()
-    picosecond_without_cheats = dijkstras(maze)
+def dijkstras_from_point(maze: list[list[str]], start: tuple[int, int]):
+    q: list[RegularNode] = []
+    heapq.heappush(q, RegularNode(0, start[0], start[1]))
 
-    start, end = find_start_and_end_points(maze)
-
-    counter = Counter()
-    visited = set()
-
-    q: list[CheatNode] = []
-    heapq.heappush(q, CheatNode(0, start[0], start[1], 20))
+    dd = defaultdict(lambda: float("inf"))
+    dd[start] = 0
 
     while q:
-        dist, x, y, attempts_remaining = heapq.heappop(q)
-
-        if (x, y) == end:
-            counter[dist] += 1
-            continue
-
-        if (x, y, attempts_remaining) in visited:
-            continue
-
-        visited.add((x, y, attempts_remaining))
+        dist, x, y = heapq.heappop(q)
 
         for dx, dy in DIRECTIONS:
             nx, ny = x + dx, y + dy
+
             if (
                 nx < 0
                 or ny < 0
                 or nx >= len(maze[0])
                 or ny >= len(maze)
-                or (nx, ny, attempts_remaining) in visited
+                or maze[ny][nx] == "#"
             ):
                 continue
 
-            is_wall = maze[ny][nx] == "#"
+            alt = dist + 1
+            if alt <= dd[(nx, ny)]:
+                dd[(nx, ny)] = alt
+                heapq.heappush(q, RegularNode(alt, nx, ny))
 
-            if attempts_remaining > 0 and is_wall:
-                heapq.heappush(
-                    q,
-                    CheatNode(dist + 1, nx, ny, attempts_remaining - 1),
-                )
+    return dd
 
-            if not is_wall:
-                heapq.heappush(
-                    q,
-                    CheatNode(dist + 1, nx, ny, attempts_remaining),
-                )
+
+def part2():
+    """
+    The idea with this solution is to find all the distances from the start to every point
+    and also to find the distances from the end to every point
+
+    With this information, we can find out if its possible to reach any two points
+    within 20 cheats.
+
+    Then we should check if (S -> A) + (A -> B) + (B -> E) will be less than the
+    optimal answer - 100
+    """
+    maze = parse_input()
+    start, end = find_start_and_end_points(maze)
+    dd_from_start = dijkstras_from_point(maze, start)
+    dd_from_end = dijkstras_from_point(maze, end)
 
     res = 0
 
-    for key, value in counter.items():
-        if key != picosecond_without_cheats:
-            print(
-                f"There are {value} cheats that save {picosecond_without_cheats - key} picoseconds"
-            )
-        # if key != picosecond_without_cheats and picosecond_without_cheats - key >= 100:
-        #     res += value
+    for x, y in dd_from_start.keys():
+        for nx, ny in dd_from_end.keys():
+            if (diff := abs(nx - x) + abs(ny - y)) <= 20:
+                if (
+                    dd_from_start[(x, y)] + diff + dd_from_end[(nx, ny)]
+                    <= dd_from_start[end] - 100
+                ):
+                    res += 1
 
     return res
 
---
+++ advent_of_code/yr2024/day21/main.py
@@ -0,0 +1,17 @@
+import os
+
+PATH_TO_FILE = f"{os.path.dirname(os.path.realpath(__file__))}/data.in"
+
+
+def parse_input():
+    with open(PATH_TO_FILE, "r") as f:
+        return f.read().strip().splitlines()
+
+
+def part1():
+    seqs = parse_input()
+
+
+if __name__ == "__main__":
+    print("Advent of Code 2024 - Day 21")
+    print(f"Part 1: {part1()}")
