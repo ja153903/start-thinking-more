a1a7867f89d2c362aae8d24480e731ce7c2524eb
ja153903 <jaimeabbariao@gmail.com>
2024-12-29 21:23:59 -0500

    updating with a leetcode problem

---
+++ leetcode/p2466.py
@@ -0,0 +1,22 @@
+from functools import lru_cache
+
+
+MOD = 10**9 + 7
+
+
+class Solution:
+    # TODO: This solution fails time limit constraints, but is a workable solution
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        @lru_cache
+        def inner(path: str) -> int:
+            if len(path) > high:
+                return 0
+
+            res = 0
+
+            if low <= len(path) <= high:
+                res += 1
+
+            return res + inner(path + "0" * zero) + inner(path + "1" * one)
+
+        return inner("") % MOD
