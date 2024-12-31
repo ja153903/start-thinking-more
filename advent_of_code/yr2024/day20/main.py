f0fcc962889548e09c14614ec7025f67f52cbd18
ja153903 <jaimeabbariao@gmail.com>
2024-12-31 00:30:07 -0500

    updating print statement here

--- advent_of_code/yr2024/day20/main.py
+++ advent_of_code/yr2024/day20/main.py
@@ -164,5 +164,5 @@ def part2():
 
 if __name__ == "__main__":
     print("Advent of Code 2024 - Day 20")
-    # print(f"Part 1: {part1()}")
+    print(f"Part 1: {part1()}")
     print(f"Part 2: {part2()}")
