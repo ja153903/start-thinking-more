518981dfa4877fb54a5420383b697526fa7f0ba7
ja153903 <jaimeabbariao@gmail.com>
2025-01-18 22:15:59 -0500

    solving some more problems

--- advent_of_code/yr2015/day13/main.py
+++ advent_of_code/yr2015/day13/main.py
@@ -1,7 +1,4 @@
 import os
-import re
-from typing import NamedTuple
-from collections import defaultdict
 
 PATH_TO_FILE = f"{os.path.dirname(os.path.realpath(__file__))}/data.in"
 
@@ -11,37 +8,12 @@ def parse_input():
         return f.read().strip().splitlines()
 
 
-HAPPINESS_REGEX = re.compile(
-    r"(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+)."
-)
-
-
-class Edge(NamedTuple):
-    u: str
-    v: str
-    w: int
-
-
-def parse_edge(line: str) -> Edge:
-    u, sign, w, v = [part for part in re.split(HAPPINESS_REGEX, line) if part]
-    return Edge(u, v, -int(w) if sign == "lose" else int(w))
-
-
 def part1():
-    edges = [parse_edge(line) for line in parse_input()]
-    edge_mp = defaultdict(int)  # key => (u, v) and (v, u), value => weight
-    g = defaultdict(list)
-
-    for edge in edges:
-        g[edge.u].append(edge.v)
-        g[edge.v].append(edge.u)
-        edge_mp[(edge.u, edge.v)] += edge.w
-        edge_mp[(edge.v, edge.u)] += edge.w
-
     raise NotImplementedError
 
 
-def part2(): ...
+def part2():
+    raise NotImplementedError
 
 
 if __name__ == "__main__":
---
+++ leetcode/p1400.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        counts = Counter(s)
+
+        return False
