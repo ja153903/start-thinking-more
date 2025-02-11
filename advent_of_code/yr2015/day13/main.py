af1d32dc8da13bed526dc9d4c21e548dfb7cdc54
ja153903 <jaimeabbariao@gmail.com>
2025-02-11 12:45:31 -0500

    updating with solution to 2015 day 13

--- advent_of_code/yr2015/day13/main.py
+++ advent_of_code/yr2015/day13/main.py
@@ -1,6 +1,13 @@
+from itertools import permutations
 import os
+import re
+from collections import defaultdict
 
 PATH_TO_FILE = f"{os.path.dirname(os.path.realpath(__file__))}/data.in"
+
+HAPPINESS_REGEX = re.compile(
+    r"(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+)."
+)
 
 
 def parse_input():
@@ -8,12 +15,53 @@ def parse_input():
         return f.read().strip().splitlines()
 
 
+def parse_data():
+    data = parse_input()
+    dd = defaultdict(lambda: defaultdict(int))
+
+    for line in data:
+        parts = [part for part in re.split(HAPPINESS_REGEX, line) if part]
+        a, wl, n, b = parts
+        dd[a][b] = int(n) if wl == "gain" else -int(n)
+
+    return dd
+
+
+def calculate_happiness(dd, people) -> int:
+    happiness = 0
+    for i in range(1, len(people)):
+        a, b = people[i - 1], people[i]
+        happiness += dd[a][b]
+        happiness += dd[b][a]
+
+    happiness += dd[people[0]][people[-1]]
+    happiness += dd[people[-1]][people[0]]
+
+    return happiness
+
+
 def part1():
-    raise NotImplementedError
+    dd = parse_data()
+    people = list(dd.keys())
+
+    return max(
+        calculate_happiness(dd, people_perm) for people_perm in permutations(people)
+    )
 
 
 def part2():
-    raise NotImplementedError
+    dd = parse_data()
+    people = list(dd.keys())
+
+    for person in people:
+        dd[person]["me"] = 0
+        dd["me"][person] = 0
+
+    people.append("me")
+
+    return max(
+        calculate_happiness(dd, people_perm) for people_perm in permutations(people)
+    )
 
 
 if __name__ == "__main__":
